016b20183a7e82aa7fca9b9c6704a9406d03864e
JEverhart383 <jeffeverhart383@gmail.com>
2015-06-01 09:35:47 -0400

    update

--- script.js
+++ script.js
@@ -1,51 +1,55 @@
 
 //Spring 2015 
-var Spring2015 = "data/Spring2015.json"; 
+var Spring2015 = "data/Spring2015/Spring2015"; 
 var Spring2015Name = "Spring 2015"; 
 var Spring2015StartDate = "2015-01-11"; 
 var Spring2015EndDate = "2015-05-03" ; 
 
 //Fall 2014 
-var Fall2014 = "data/Fall2014.json"; 
+var Fall2014 = "data/Fall2014/Fall2014"; 
 var Fall2014Name = "Fall 2014";
 var Fall2014StartDate = "2014-08-24"; 
 var Fall2014EndDate = "2014-12-14"; 
 
 //Summer II 2014
-var Summer22014 = "data/Summer22014.json";
+var Summer22014 = "data/Summer22014/Summer22014";
 var Summer22014Name = "Summer II 2014";
 var Summer22014StartDate = "2014-05-18" ; 
 var Summer22014EndDate = "2014-08-03" ;  
 
 //Summer I 2014
-var Summer12014 = "data/Summer12014.json"; 
+var Summer12014 = "data/Summer12014/Summer12014"; 
 var Summer12014Name = "Summer I 2014"; 
 var Summer12014StartDate = "2014-05-18" ; 
 var Summer12014EndDate = "2014-07-02" ; 
 
 //Spring 2014
-var Spring2014 = "data/Spring2014.json"; 
+var Spring2014 = "data/Spring2014/Spring2014"; 
 var Spring2014Name = "Spring 2014"; 
 var Spring2014StartDate = "2014-01-12" ; 
 var Spring2014EndDate = "2014-05-31" ; 
 
 //Fall 2013
-var Fall2013 = "data/Fall2013.json"; 
+var Fall2013 = "data/Fall2013/Fall2013"; 
 var Fall2013Name = "Fall 2013"; 
 var Fall2013StartDate = "2013-08-25" ; 
 var Fall2013EndDate = "2013-12-29" ; 
 
 //Summer II 2013
-var Summer22013 = "data/Summer22013.json";
+var Summer22013 = "data/Summer22013/Summer22013";
 var Summer22013Name = "Summer II 2013"; 
 var Summer22013StartDate = "2013-05-12" ; 
 var Summer22013EndDate = "2013-08-31" ; 
 
 //Summer I 2013
-var Summer12013 = "data/Summer12013.json";
+var Summer12013 = "data/Summer12013/Summer12013";
 var Summer12013Name = "Summer I 2013"; 
 var Summer12013StartDate = "2013-05-12" ; 
 var Summer12013EndDate = "2013-06-30" ; 
+
+//Category Overview 
+
+var catOverviewPath = "data/CategoryTotal.json"; 
 
 
 //define function to write charts with data path and term name as variables
@@ -55,11 +59,13 @@ function createCharts(dataPath, termName, startDate, endDate){
 
 	//remove SVGs if updating data 
 	d3.select("#chart svg").remove("svg"); 
-	d3.select("#chart2 svg").remove("svg"); 
+	d3.select("#chart2 svg").remove("svg");
+	d3.select("#gradeChart svg").remove("svg");  
 
 	//create SVGs 
 	var svg = dimple.newSvg("#chart", 1200, 500);
-	var svg2 = dimple.newSvg("#chart2", 1000, 500);
+	var svg2 = dimple.newSvg("#chart2", 800, 500);
+	var svg3 = dimple.newSvg("#gradeChart", 450, 200); 
 	
 	//adjust start and end dates for 2 week window 
 	startDate = new Date(startDate); 
@@ -68,10 +74,12 @@ function createCharts(dataPath, termName, startDate, endDate){
 	endDate = new Date(endDate); 
 	endDate = new Date(endDate + 1209600000); 
 
-	console.log(startDate + " to " + endDate); 
-	
+	var activityPath = dataPath + "Activity.json"; 
+	var statisticsPath = dataPath + "Statistics.json"; 
+	var gradesPath = dataPath + "Grades.json"
 
-	d3.json(dataPath, function(data){  
+	//load JSON for Activity path 
+	d3.json(activityPath, function(data){  
 
 			d3.selectAll(".term-name").text(termName); 
 			var data1 = data.by_date; 
@@ -117,21 +125,94 @@ function createCharts(dataPath, termName, startDate, endDate){
 			var s = myChart.addSeries(null , dimple.plot.line); 
 			myChart.draw(); 
 
+
 			var myChart2 = new dimple.chart(svg2, data2);
-			myChart2.setBounds(60,30,950,450); 
+			myChart2.setBounds(30,30,750,450); 
 			myChart2.addMeasureAxis("p", "views"); 
 			var ring = myChart2.addSeries("category", dimple.plot.pie); 
 			ring.innerRadius = "50%"; 
-			myChart2.addLegend(150,20,200,300, "left"); 
+			myChart2.addLegend(30,20,200,300, "left"); 
 			myChart2.draw(); 
 
 	});//End JSON function
 
+
+	//Begin JSON load for stats totals 
+	d3.json(statisticsPath, function(data){
+		d3.select(".total-courses").text(data.courses); 
+		d3.select(".total-teachers").text(data.teachers); 
+		d3.select(".total-students").text(data.students);
+		d3.select(".total-discussions").text(data.discussion_topics);
+		d3.select(".total-media").text(data.media_objects);
+		d3.select(".total-attachments").text(data.attachments);
+		d3.select(".total-assignments").text(data.assignments);
+
+	}); 
+
+	//Begin JSON load for grades 
+
+	d3.json(gradesPath, function(data){
+
+		gradesArray = [];
+
+		function gradeScore(score, frequency){
+			this.score = score;
+			this.frequency = frequency; 
+		} 
+
+		for (var key in data){
+			var newGrade = new gradeScore(parseInt(key), data[key]); 
+			gradesArray.push(newGrade);  
+		}
+
+		console.log(gradesArray); 
+		 
+
+		var myChart3 = new dimple.chart(svg3, gradesArray); 
+			
+			myChart3.setBounds(50, 15, 350, 120); 
+			var x = myChart3.addCategoryAxis("x", "score");   
+			x.hidden = false;
+			x.title = "Score"; 
+			var y = myChart3.addCategoryAxis("y", "frequency"); 
+			y.title = "Frequency"; 
+			var s = myChart3.addSeries(null , dimple.plot.line); 
+			myChart3.draw();    
+			
+			d3.selectAll("#gradeChart svg g .dimple-axis g text").remove("text"); 
+
+
+	}); 
+
+
+
+
 }//End createCharts 
+
+function categoryOverview(catDataPath){
+
+	var svg = dimple.newSvg("#chart3", 1000, 500);
+    d3.json(catDataPath, function (data) { 	
+
+      var myChart = new dimple.chart(svg, data); 
+      myChart.setBounds(65, 30, 950, 400)
+      myChart.addCategoryAxis("x", "term_name");
+      myChart.addMeasureAxis("y", "views");
+      myChart.addMeasureAxis("z", "views");
+      myChart.addSeries("category", dimple.plot.bubble);
+      myChart.addLegend(900, 40, 200, 200, "left");
+      myChart.draw();
+    });
+
+
+}
+
 
 //inital data load with most recent completed term
 
 createCharts(Spring2015, Spring2015Name, Spring2015StartDate, Spring2015EndDate); 
+
+categoryOverview(catOverviewPath); 
 
 
 //update data with Spring 2015 
